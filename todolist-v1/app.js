b33fbd5ae996a3bc18984e8b2bb4d5f27256c82d
d_flex <hiyoo95@gmail.com>
2022-07-11 19:24:38 +0900

    header and footer separated from list.ejs with layout

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -10,6 +10,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(`${__dirname}/public`));
+
 let items = [];
 
 app.get('/', (req, res) => {
@@ -23,10 +25,16 @@ app.get('/', (req, res) => {
   res.render('list', { today, items });
 });
 
-app.post('/', (req, res) => {
+app.post('/add', (req, res) => {
   const { newItem } = req.body;
   items.push(newItem);
 
+  res.redirect('/');
+});
+
+app.post('/delete', (req, res) => {
+  const { deleteItem } = req.body;
+  items = items.filter((item) => item !== deleteItem);
   res.redirect('/');
 });
 
