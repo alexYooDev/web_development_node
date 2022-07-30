483b7fd35f9db8a3167ffd2ebde00a19ae96c5ad
d_flex <hiyoo95@gmail.com>
2022-07-31 00:11:21 +0900

    CRUD operation with mongoose

--- fruit-project/app.js
+++ fruit-project/app.js
@@ -1,32 +1,106 @@
 //jshint esversion:6
 
 const { mongoose } = require('mongoose');
-
-//testing
+const { Schema, model } = require('mongoose');
 
 const url = 'mongodb://localhost:27017';
 
 const dbName = 'fruit-db';
 
-// const client = new MongoClient(url);
 
-// client.connect((err) => {
-//   assert.equal(null, err);
-//   console.log('Connected successfully to server.');
+const fruitSchema = new Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
 
-//   const db = client.db(dbName);
-
-//   findDocuments(db, () => {
-//     client.close();
-//   })
-// });
+const Fruit = model('Fruit', fruitSchema);
 
 const connectDb = async () => {
   await mongoose.connect(`${url}/${dbName}`);
   console.log('Database successfully connected!');
 }
 
-connectDb().catch(error => console.log(error));
+const createDocuments = async (callback) => {
+
+  // Data Schema
+
+  // Creating model
+
+  // const fruit = new Fruit({
+  //   name: 'Apple',
+  //   rating: 7,
+  //   review: 'Great for the start of the day.',
+  // });
+
+  // await fruit.save();
+
+  // const personSchema = new Schema({
+  //   name: String,
+  //   age: Number
+  // });
+
+  // const Person = model('Person', personSchema);
+
+  // const person = new Person({
+  //   name: 'John',
+  //   age: 37
+  // })
+
+  // await person.save();
+
+  await callback();
+};
+
+const createFruits = async (callback) => {
+  const kiwi = new Fruit({
+    name: 'Kiwi',
+    score: 6,
+    review: 'Kinda sour fruit'
+  });
+
+  const orange = new Fruit({
+    name: 'Orange',
+    score: 8,
+    review: 'Great for morning juice'
+  });
+
+  const banana = new Fruit({
+    name: 'Banana',
+    score: 5,
+    review: 'Decent food supplement'
+  });
+
+  Fruit.insertMany([kiwi,orange,banana], (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('successfully added all the fruits to db')
+    }
+  })
+}
+
+const findFruits = async () => {
+  await Fruit.find((error, fruits) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(fruits);
+    }
+  })
+}
+
+connectDb().catch((error) => {
+  console.log(error);
+});
+
+findFruits();
+
+// createFruits(() => {
+//   console.log('Successfully inserted documents!');
+// });
+
+
 
 const insertDocuments = async (db, callback) => {
   const collection = db.collection('fruits');
