ec75c7fb1c901681e0ac4fc28a2c63ee43eac9e1
d_flex <hiyoo95@gmail.com>
2022-07-31 14:47:15 +0900

    todolist project upate to v2 : connecting to mongo db using mongoose instead of locally created variable data container

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,20 +1,33 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
+const date = require(`${__dirname}/date.js`);
+const mongoose = require('mongoose');
+const {Schema, model} = require('mongoose');
 
 const app = express();
-
-const date = require(`${__dirname}/date.js`);
 
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(`${__dirname}/public`));
 
-let items = [];
+// let items = [];
+
+const dbName = 'todolistDB';
+const url = `mongodb://localhost:27017`;
+
+mongoose.connect(`${url}/${dbName}`);
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'No entry specified, please check your data entry.']
+  }
+})
+
+const Item = model('item', itemSchema);
 
 app.get('/', (req, res) => {
   const today = date.getTodayDate();
