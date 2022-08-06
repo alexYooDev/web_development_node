6dad62ad09633f7e892ac17ecc1e8ebc9361721a
d_flex <hiyoo95@gmail.com>
2022-08-06 22:42:18 +0900

    level 1 user authentication : matching  email and password

--- secrets/app.js
+++ secrets/app.js
@@ -1,1 +1,74 @@
-//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const { model, Schema } = require('mongoose');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/user-db');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = model('User', userSchema);
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+
+  const newUser = new User({email: username, password});
+
+  newUser.save((error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.render('secrets');
+    }
+  })
+})
+
+app.post('/login', (req, res) => {
+  const {username, password} = req.body;
+
+  User.findOne({username, password}, (error, result) => {
+    if (error) {
+      console.log(error);
+    } 
+
+    if (result.length !== 0) {
+      console.log(`Found User! ${result}`);
+    } else {
+      console.log('No user found under that email and password');
+    }
+  })
+})
+
+app.listen(PORT, () => {
+  console.log('The server is running on the port: ' + PORT)
+})
